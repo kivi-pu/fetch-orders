386a0f0cdc232e8440988d9031436283eac71d1e
Just Me <just806me@gmail.com>
2021-03-03 23:44:12 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/my-pricing-test/fetch-orders
+
+go 1.15
+
+require (
+	cloud.google.com/go/firestore v1.5.0 // indirect
+	cloud.google.com/go/storage v1.14.0 // indirect
+	firebase.google.com/go v3.13.0+incompatible // indirect
+	golang.org/x/oauth2 v0.0.0-20210220000619-9bb904979d93 // indirect
+	google.golang.org/api v0.40.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"os"
+	"time"
+
+	"context"
+
+	"cloud.google.com/go/firestore"
+	firebase "firebase.google.com/go"
+
+	"google.golang.org/api/option"
+)
+
+type product struct {
+	XMLName xml.Name `xml:"product"`
+	ID      string   `json:"id" xml:"code"`
+	Amount  int      `json:"amount" xml:"amount"`
+}
+
+type order struct {
+	XMLName  xml.Name  `xml:"order"`
+	UID      string    `xml:"uid"`
+	Date     time.Time `xml:"date"`
+	Products []product `xml:"products>product"`
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Printf("Usage: %s path/to/orders.xml path/to/key.json\n(got %+v)\n", os.Args[0], os.Args[1:])
+
+		return
+	}
+
+	client, err := setupFirestore(os.Args[2])
+
+	if err != nil {
+		panic(err)
+	}
+
+	documents, err := fetchDocuments(client)
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = saveOrders(os.Args[1], parseOrders(documents))
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = deleteDocuments(client, documents)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func setupFirestore(credentialsFilename string) (*firestore.Client, error) {
+	app, err := firebase.NewApp(context.Background(), nil, option.WithCredentialsFile(credentialsFilename))
+
+	if err != nil {
+		return nil, fmt.Errorf("error initializing firebase: %v", err)
+	}
+
+	client, err := app.Firestore(context.Background())
+
+	if err != nil {
+		return nil, fmt.Errorf("error initializing firestore: %v", err)
+	}
+
+	return client, nil
+}
+
+func fetchDocuments(client *firestore.Client) ([]*firestore.DocumentSnapshot, error) {
+	orderDocuments, err := client.Collection("orders").OrderBy("date", firestore.Desc).Documents(context.Background()).GetAll()
+
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving dordersocuments: %v", err)
+	}
+
+	return orderDocuments, nil
+}
+
+func deleteDocuments(client *firestore.Client, documents []*firestore.DocumentSnapshot) error {
+	batch := client.Batch()
+
+	for _, document := range documents {
+		batch.Delete(document.Ref)
+	}
+
+	_, err := batch.Commit(context.Background())
+
+	if err != nil {
+		return fmt.Errorf("error deleting orders: %v", err)
+	}
+
+	return nil
+}
+
+func parseOrders(documents []*firestore.DocumentSnapshot) []order {
+	orders := make([]order, len(documents))
+
+	for i, document := range documents {
+		data := document.Data()
+
+		productsStr := data["products"].([]interface{})
+
+		orders[i].Products = make([]product, len(productsStr))
+
+		for j, ps := range productsStr {
+			json.Unmarshal([]byte(ps.(string)), &orders[i].Products[j])
+		}
+
+		orders[i].UID = data["uid"].(string)
+
+		orders[i].Date = data["date"].(time.Time)
+	}
+
+	return orders
+}
+
+func saveOrders(filename string, orders []order) error {
+	type output struct {
+		XMLName xml.Name `xml:"orders"`
+		Orders  []order  `xml:"order"`
+	}
+
+	data, err := xml.Marshal(output{Orders: orders})
+
+	if err != nil {
+		return fmt.Errorf("error converting to xml: %v", err)
+	}
+
+	tmpFilename := filename + ".tmp"
+
+	err = os.WriteFile(tmpFilename, data, 0644)
+
+	if err != nil {
+		return fmt.Errorf("error writing file %s: %v", tmpFilename, err)
+	}
+
+	err = os.Rename(tmpFilename, filename)
+
+	if err != nil {
+		return fmt.Errorf("error writing file %s: %v", filename, err)
+	}
+
+	return nil
+}
